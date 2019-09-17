a2fb3b529c826d0950caa972939e3e498fb592f5
Palash Nigam <npalash25@gmail.com>
2019-09-17 23:57:28 +0530

    Add version command; it works :tada:

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,5 @@
 // Tools for communicating with the FIST server
+use std::io::{Error, Write};
 use std::net::TcpStream;
 
 // The client for the FIST server.
@@ -20,13 +21,28 @@ impl FistClient {
     pub fn connect(&mut self) {
         let res = TcpStream::connect(&self.addr);
         match res {
-            Ok(c) => self.conn = Some(c),
+            Ok(c) => {
+                self.conn = Some(c);
+                println!("done\n");
+            },
             Err(_) => panic!(),
         }
+    }
+
+    // Sends the `VERSION` command to the FIST server
+    // which returns the current version of FIST
+    pub fn version(self) -> Result<usize, Error> {
+        let command_string = b"VERSION\r\n";
+        self.conn.unwrap().write(&command_string[0..])
     }
 
     // Return the address of the FIST server
     pub fn get_addr(self) -> String {
         self.addr
+    }
+
+    // Return the connection to the FIST server
+    pub fn get_conn(self) -> Option<TcpStream> {
+        self.conn
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,12 @@ pub mod client;
 mod tests {
     #[test]
     fn it_works() {
-        let fist_client = super::client::FistClient::new("127.0.0.1", "5575");
-        assert_eq!(fist_client.get_addr(), "127.0.0.1:5575");
+        let mut fist_client = super::client::FistClient::new("127.0.0.1", "5575");
+        //assert_eq!(fist_client.get_addr(), "127.0.0.1:5575");
+        fist_client.connect();
+        match fist_client.version() {
+            Ok(v) => println!("{}", v),
+            Err(_) => panic!(),
+        };
     }
 }
