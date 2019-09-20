96778105a3d63cbd81ee27b32082ecbce83f49a3
Palash Nigam <npalash25@gmail.com>
2019-09-21 00:28:31 +0530

    Add rest of the commands and tests

--- src/client.rs
+++ src/client.rs
@@ -32,12 +32,56 @@ impl FistClient {
     // Sends the `VERSION` command to the FIST server
     // which returns the current version of FIST
     pub fn version(&self) -> Result<Vec<u8>, Error> {
-        let mut ver: [u8; 8] = [0;8];
-        const COMMAND_STRING: &[u8] = b"VERSION\r\n";
+        let mut ver: [u8; 6] = [0;6];
+        const VERSION_COMMAND: &[u8] = b"VERSION\r\n";
         let mut conn = self.conn.as_ref().unwrap();
-        conn.write(COMMAND_STRING)?;
+        conn.write(VERSION_COMMAND)?;
         conn.read(&mut ver[..])?;
         Ok(ver.iter().cloned().collect())
+    }
+
+    // Sends the `INDEX` command to index some text in a document
+    pub fn index(&self, doc: &str, text: &str) -> Result<Vec<u8>, Error> {
+        let mut ver: [u8; 22] = [0;22];
+        let index_str = format!("{} {} {}\r\n", "INDEX", doc, text);
+        let index_command: &[u8] = index_str.as_bytes();
+        let mut conn = self.conn.as_ref().unwrap();
+        conn.write(index_command)?;
+        conn.read(&mut ver[..])?;
+        Ok(ver.iter().cloned().collect())
+    }
+
+    // Sends the `SEARCH` command to look for the provided text.
+    // If successful returns a list of documents in which the text
+    // was found otherwise an empty list (`[]`) is returned by the server
+    pub fn search(&self, text: &str) -> Result<Vec<u8>, Error> {
+        let mut ver: [u8; 9] = [0;9];
+        let search_str = format!("{} {}\r\n", "SEARCH", text);
+        let search_command: &[u8] = search_str.as_bytes();
+        let mut conn = self.conn.as_ref().unwrap();
+        conn.write(search_command)?;
+        conn.read(&mut ver[..])?;
+        Ok(ver.iter().cloned().collect())
+    }
+
+    // Sends the `DELETE` command to delete all the documents that
+    // contain the provided text string
+    pub fn delete(&self, text: &str) -> Result<Vec<u8>, Error> {
+        let mut ver: [u8; 8] = [0;8];
+        let delete_str = format!("{} {}\r\n", "DELETE", text);
+        let delete_command: &[u8] = delete_str.as_bytes();
+        let mut conn = self.conn.as_ref().unwrap();
+        conn.write(delete_command)?;
+        conn.read(&mut ver[..])?;
+        Ok(ver.iter().cloned().collect())
+    }
+
+    // Sends the `EXIT` command to the FIST server
+    pub fn exit(&self) -> Result<usize, Error> {
+        const EXIT_COMMAND: &[u8] = b"EXIT\r\n";
+        let mut conn = self.conn.as_ref().unwrap();
+        let res = conn.write(EXIT_COMMAND)?;
+        Ok(res)
     }
 
     // Return the address of the FIST server
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,36 @@
 pub mod client;
 
-
 #[cfg(test)]
 mod tests {
     #[test]
 
     fn it_works() {
         let mut fist_client = super::client::FistClient::new("127.0.0.1", "5575");
-        //assert_eq!(fist_client.get_addr(), "127.0.0.1:5575");
         fist_client.connect();
         match fist_client.version() {
-            // println!("{} {} {} {} {} {} {} {}", v[0] as char, v[1], v[2], v[3], v[4], v[5], v[6], v[7])
             Ok(v) => {
-                let response = String::from_utf8(v).unwrap();
-                println!("{}", response);
+                assert_eq!(String::from_utf8(v).unwrap(), "0.0.2\n")
+            }
+            Err(_) => panic!(),
+        };
+
+        match fist_client.index("doc1", "A string to index") {
+            Ok(v) => {
+                assert_eq!(String::from_utf8(v).unwrap(), "Text has been indexed\n");
+            }
+            Err(_) => panic!(),
+        };
+
+        match fist_client.search("string to index") {
+            Ok(v) => {
+                assert_eq!(String::from_utf8(v).unwrap(), "[\"doc1\"]\n");
+            }
+            Err(_) => panic!(),
+        };
+
+        match fist_client.exit() {
+            Ok(v) => {
+                assert_eq!(v, 6);
             }
             Err(_) => panic!(),
         };
